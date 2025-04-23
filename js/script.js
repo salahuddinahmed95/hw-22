ebf4aad28d882ebf6f88e4a884b0a3682009afb9
Salahuddin Ahmed <salahuddinbdm@gmail.com>
2025-04-23 22:07:41 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// JAVA Script
+// ex-1
+function showDate(){
+  document.getElementById('sd').innerHTML = Date();
+}
+// Ex-2
+function showText(){
+  document.getElementById('st').innerHTML=Text();
+}
+// ex-3
+function changeText(){
+  document.getElementById('ct').
+innerHTML = "JAVA SCRIPT";
+}
+// ex-4
+
+function bulbOn(){
+  document.getElementById('img').
+src = "image/pic_bulbon.gif";
+}
+function bulbOff(){
+  document.getElementById
+('img').
+src = "image/pic_bulboff.gif";
+}
+// ex-5
+function changeColor(){
+  document.getElementById('cc').style.backgroundColor="green";
+}
+
+// Ex-6
+function showText(){
+  document.getElementById('sohi').style.display="block";
+}
+
+function hideText(){
+  document.getElementById('sohi').style.display="none";
+}
